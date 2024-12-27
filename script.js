6fa6c82a4d11a4a7985705d3b12f2eddf82a3405
Kevin Åhlin <kevin.ahlin@hotmail.com>
2024-12-27 15:08:17 +0100

    added function to swtich between login and registration forms

---
+++ script.js
@@ -0,0 +1,13 @@
+// HTML elements
+const wrapper = document.querySelector(".wrapper")
+const loginLink = document.querySelector(".login-link")
+const registerLink = document.querySelector(".register-link")
+
+// Eventlisteners and functions
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active")
+})
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active")
+})
