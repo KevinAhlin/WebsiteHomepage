03dbd7b2530da6293c3dcad9a733d0fa262902aa
Kevin Åhlin <kevin.ahlin@hotmail.com>
2024-12-27 15:17:07 +0100

    added js, so that login button shows the login form

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const wrapper = document.querySelector(".wrapper")
 const loginLink = document.querySelector(".login-link")
 const registerLink = document.querySelector(".register-link")
+const btnPopup = document.querySelector(".btnLogin-popup")
 
 // Eventlisteners and functions
 registerLink.addEventListener("click", () => {
@@ -10,4 +11,8 @@ registerLink.addEventListener("click", () => {
 
 loginLink.addEventListener("click", () => {
   wrapper.classList.remove("active")
+})
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup")
 })
